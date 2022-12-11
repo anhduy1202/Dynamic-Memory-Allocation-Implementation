d0946f378709cce91250438622e62a77b61c08df
Daniel Truong <aduy1122@gmail.com>
2022-12-11 05:28:21 +0000

    change something idk

--- .ccls-cache/@home@runner@Big-Project-1-Daniel-Truong/block_list.h
+++ .ccls-cache/@home@runner@Big-Project-1-Daniel-Truong/block_list.h
@@ -1,15 +1,28 @@
 
 #include <stdlib.h>
 
-typedef struct block_list {
+typedef struct memory_block {
   void *block_base;
   size_t block_size;
-} BlockEntry, *BlockList;
+} Block;
+
+typedef struct block_list {
+  int length;
+  Block *blocks;
+} BlockEntry;
 
 // ASSUME that block list is a fixed size array
-// BLOCK_LIST STUFF:
 
+// BLOCK_LIST STUFF:
 // - ADD ENTRY (base pointer, size)
+void *add_entry(void *base_pointer, Block block) {
+  //   if number of entries in list is less than list size
+  //    create entry
+  //    add at end
+  //    increase number of entries in list
+  //   return
+  // return LIST_FULL
+}
 // - REMOVE ENTRY (base pointer)
 // - FIND BY BASE PTR (test_value)
 // - FIND SIZE GREATER EQUAL THAN (test_size)
